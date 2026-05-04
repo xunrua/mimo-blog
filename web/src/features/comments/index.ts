d74e184ca3141cc2b2a3189a8409cbc51d249175
meyou <2636699780@qq.com>
2026-05-04 20:27:35 +0800

    chore(comments): 清理旧组件并更新导出
    - 删除废弃组件：
      - CommentInput.tsx
      - EmojiPicker.tsx
      - EmojiPopover.tsx
      - EmojiPickerButton.tsx
    - 更新 index.ts 导出新组件
    - 导出 RichTextInputRef 类型

--- web/src/features/comments/index.ts
+++ web/src/features/comments/index.ts
@@ -5,11 +5,17 @@
 // 组件
 export { CommentSection } from "./components/CommentSection";
 export { CommentItem } from "./components/CommentItem";
-export { EmojiPicker } from "./components/EmojiPicker";
-export { EmojiPickerButton } from "./components/EmojiPickerButton";
+export { CommentForm } from "./components/CommentForm";
+export { RichTextInput } from "./components/RichTextInput";
+export { EmojiButton } from "./components/EmojiButton";
+export { EmojiPanel } from "./components/EmojiPanel";
+export { EmojiItem } from "./components/EmojiItem";
+export { ReactionBar } from "./components/ReactionBar";
+export { ReplyIndicator } from "./components/ReplyIndicator";
 
 // API & Hooks
 export { useComments, useSubmitComment } from "./api";
 
 // Types
-export type { Comment, CommentSubmitData } from "./types";
+export type { Comment, CommentSubmitData, CommentReaction, CommentWithReactions } from "./types";
+export type { RichTextInputRef } from "./components/RichTextInput";
